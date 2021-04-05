99b4391d25219ee267f1257bb2edc13750bc1170
Juan Guzman <juan.guzman.sd@gmail>
2021-04-05 11:57:23 -0500

    Updates

---
+++ precache-manifest.ea01a2d4261b22e9c4459691314b9669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e549bd720e796e3ae396edad401ff5",
+    "url": "/redux-meme-generator/index.html"
+  },
+  {
+    "revision": "abf5df65a4abb2c69317",
+    "url": "/redux-meme-generator/static/css/main.efd08f97.chunk.css"
+  },
+  {
+    "revision": "38fde5661035718ea441",
+    "url": "/redux-meme-generator/static/js/2.d0b07959.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/redux-meme-generator/static/js/2.d0b07959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf5df65a4abb2c69317",
+    "url": "/redux-meme-generator/static/js/main.24a38492.chunk.js"
+  },
+  {
+    "revision": "ab7294f3ff98dc285e6f",
+    "url": "/redux-meme-generator/static/js/runtime-main.e4ab6285.js"
+  }
+]);
